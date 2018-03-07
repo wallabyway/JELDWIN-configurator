a0916c56dd252598d7f0e03e73acfe7c450134d5
Michael Beale <michael.beale@autodesk.com>
2018-03-07 14:38:16 -0800

    initial configurator

---
+++ docs/app.js
@@ -0,0 +1,87 @@
+var viewer;
+var markup;
+var DBURL = 'http://localhost:3000';
+
+// Vue.js components
+window.app = new Vue({
+  el: "#app",
+  
+  data: {
+    homeViewState: {"seedURN":"svf/AuroraDoor/Design.svf","objectSet":[{"id":[],"isolated":[],"hidden":[],"explodeScale":0,"idType":"lmv"}],"viewport":{"name":"","eye":[2857.987434680486,-810.5064192850126,7738.767479059014],"target":[0.0000010000003385357559,0.00518798828125,-8.06173062324524],"up":[0.03381201771509838,0.9952170298819234,0.09165048221933624],"worldUpVector":[0,1,2.560839e-16],"pivotPoint":[9.999999974752427e-7,0.00518798828125,-8.06173062324524],"distanceToOrbit":8296.890022206308,"aspectRatio":1.8694481830417227,"projection":"perspective","isOrthographic":false,"fieldOfView":18.352245604077158},"renderOptions":{"environment":"Warm Light","ambientOcclusion":{"enabled":true,"radius":60,"intensity":0.4},"toneMap":{"method":1,"exposure":-9,"lightMultiplier":-1e-20},"appearance":{"ghostHidden":false,"ambientShadow":false,"antiAliasing":true,"progressiveDisplay":false,"swapBlackAndWhite":false,"displayLines":true,"displayPoints":false}},"cutplanes":[]},
+    selected: [ ],
+    styleIcon: {
+      backgroundPosition:0,
+      borderRadius:"20px"
+    },
+    name: '',
+    item: {
+      title:"myTitle",
+      description:"myDesc",
+      markupId:0,
+      type:0,
+    },
+    itemIsVisible:false,
+    Items: [ { title: 'loading...', url:"" }]
+  },
+  methods: {
+    init: function() {
+      //this.loadData();
+      this.initializeViewer(this);
+    },
+
+    onSuccess: function() {
+      viewer.restoreState(this.homeViewState); 
+      viewer.setGroundReflection(false);
+      viewer.impl.toggleShadows(true);
+      viewer.impl.setShadowLightDirection(new THREE.Vector3(-10,10,10));
+    },
+
+    initializeViewer: function(self) {
+      avp.ENABLE_DEBUG=true
+
+      viewer = new Autodesk.Viewing.Private.GuiViewer3D(document.getElementById('forgeViewer'), {});
+      var options = {
+          env: "Local",
+          useADP: false,
+          useConsolidation: true,
+          urn: "svf/AuroraDoor/Design.svf"
+      }
+      Autodesk.Viewing.Initializer( options, function() {
+          viewer.start(options.urn, options, self.onSuccess);            
+      });
+    },
+
+    removeItem: function (item) {
+      this.Items = this.Items.filter( i => {return i.MarkupID != item});
+      //@@@ To Do:  Add api to remove item from MySQL
+    },
+
+    cardClick: function (item) {
+      //load measurement
+      if (!item.completed) {
+        viewer.dispatchEvent(new CustomEvent('removeData', {'detail': item.MarkupID}));
+      } else {
+        // mark all items with ID from database.  This will be used to remove items and set hover effects
+        Object.keys(item.json).map(i=>item.json[i].id=item.MarkupID);
+        viewer.dispatchEvent(new CustomEvent('newData', {'detail': item.json}));
+      }
+    },
+
+    loadData: function() {
+      //uncomment this out at activate live MySQL data
+//      fetch(`${DBURL}/allMarkup?approvalid=1`).then(r => r.json()).then( data=> {
+      fetch(`dummydata.json`).then(r => r.json()).then( data=> {
+          this.Items = data;
+          this.Items.forEach(i => i.json=JSON.parse(i.json));
+      })
+    }
+  },
+
+  computed: {
+    totalSqrFeet() {
+      return this.Items
+        .filter(i => i.completed )
+        .reduce((acc, val) => acc + val.sqrfoot, 0 );
+    },
+  }
+})
